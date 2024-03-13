f7257e291ed1815b4b18cba03746f1f57d10b8a7
dohyeon <businessdohyeon@gmail.com>
2024-03-14 07:33:18 +0900

    #15439

---
+++ #15439.cpp
@@ -0,0 +1,25 @@
+/*
+BaekJoon No
+#15439
+*/
+
+#include <iostream>
+
+using namespace std;
+
+void setDefault(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+
+int main(){
+    setDefault();
+
+    int n;
+    cin >> n;
+
+    cout << n*(n-1);
+
+    return 0;
+}
