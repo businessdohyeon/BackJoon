9fff73b5e24aabf3bcd5e7a085ec568056326db0
dohyeon <businessdohyeon@gmail.com>
2024-06-21 12:44:51 +0900

    #11404

--- #11404.cpp
+++ #11404.cpp
@@ -5,63 +5,54 @@ BackJoon No.
 
 #include <iostream>
 #include <vector>
-#include <queue>
 #include <algorithm>
 
 using namespace std;
-
-int n, m;
-// { to , weight }
-vector<pair<int,int> > graph[101];
-
-int dijkstra(int from, int to){
-    // { -cost, node }
-    priority_queue<pair<int, int> > pq;
-    pq.push(make_pair(0, from));
-
-    int minCost[n+1];
-    fill(minCost, minCost+n+1, -1);
-    minCost[from] = 0;
-
-    while(!pq.empty()){
-        pair<int, int> front = pq.top();
-        int curCost = -front.first;
-        int curNode = front.second;
-        pq.pop();
-
-        if(curNode == to)   break;
-
-        for(auto e : graph[curNode]){
-            int cal = curCost + e.second;
-            int nextNode = e.first;
-
-            if(minCost[nextNode] != -1 && cal >= minCost[nextNode]) continue;
-
-            minCost[nextNode] = cal;
-            pq.push(make_pair(-cal, nextNode));
-        }
-    }
-
-    int result = minCost[to] == -1 ? 0 : minCost[to];
-    return result;
-}
 
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
 
+    int n, m;
     cin >> n >> m;
-    
+
+    // { to , weight }
+    vector<pair<int,int> > graph[n+1];    
     for(int i=0; i<m; ++i){
         int v1, v2, weight;
         cin >> v1 >> v2 >> weight;
         graph[v1].push_back(make_pair(v2, weight));
     }
 
-    for(int i=1; i<=n; ++i){
-        for(int j=1; j<=n; ++j)
-            cout << dijkstra(i, j) << ' ';
+    int result[n+1][n+1];
+    for(int row=1; row<=n; ++row)
+        fill(result[row], result[row]+n+1, -1);
+
+    for(int node=1; node<=n; ++node){
+        result[node][node] = 0;
+        for(auto e : graph[node]){
+            if(result[node][e.first] != -1 && e.second >= result[node][e.first])    continue;
+            result[node][e.first] = e.second;
+        }
+    }
+    
+    for(int mid=1; mid<=n; ++mid){
+        for(int from=1; from<=n; ++from){
+            for(int to=1; to<=n; ++to){
+                if(result[from][mid] == -1 || result[mid][to] == -1)    continue;
+
+                int cal = result[from][mid]+result[mid][to];
+                if(result[from][to] == -1 || cal < result[from][to])
+                    result[from][to] = cal;
+            }
+        }
+    }
+
+    for(int from=1; from<=n; ++from){
+        for(int to=1; to<=n; ++to)
+            if(result[from][to] == -1)  cout << 0 << ' ';
+            else                        cout << result[from][to] << ' ';
         cout << '\n';
     }
 
