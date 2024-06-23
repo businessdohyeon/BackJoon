8bb3845d1c91360280f1e67680f4b26d39357ba7
dohyeon <businessdohyeon@gmail.com>
2024-06-23 21:58:40 +0900

    #10171

---
+++ #10171.cpp
@@ -0,0 +1,21 @@
+/*
+BackJoon No.
+#10171
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    cout << "\\    /\\" << '\n';
+    cout << " )  ( \')" << '\n';
+    cout << "(  /  )" << '\n';
+    cout << " \\(__)|" << '\n';
+
+    return 0;
+}
