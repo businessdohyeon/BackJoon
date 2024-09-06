4ca0749eac05a44e29acce6853f54e6b5e2250f2
dohyeon <businessdohyeon@gmail.com>
2024-09-06 18:45:44 +0900

    #1271

--- #1271.cpp
+++ #1271.cpp
@@ -8,101 +8,70 @@ BackJoon No.
 
 using namespace std;
 
-class BigInt{
-private:
-	string value;
-public:
-	friend istream& operator>>(istream& is, BigInt& target){
-		is >> target.value;
-		return is;
+bool comp(string a, string b){
+	if(a.length() != b.length()){
+		return (a.length() > b.length());
 	}
 
-	friend ostream& operator<<(ostream& os, const BigInt& target){
-		os << target.value;
-		return os;
+	for(int i=0; i<a.length(); ++i){
+		if(a[i] != b[i])	return (a[i] > b[i]);
 	}
+	return true;
+}
+void trim(string& str){
+	int cnt = 0;
+	while(str[cnt] == '0')
+		++cnt;
+	str = str.substr(cnt);
 
-	void operator=(const string& str){
-		this->value = str;
+	if(str.length() == 0){
+		str = "0";
 	}
-	void operator=(const BigInt& target){
-		this->value = target.value;
-	}
+}
+void A_subract_B(string& a, string& b){
+	for(int idxA = a.length()-1, idxB = b.length()-1; idxB >=0; --idxA, --idxB){
+		a[idxA] = (a[idxA] - b[idxB]) + '0';
 
-	bool operator>=(const BigInt& target){
-		if(this->value.size() != target.value.size()){
-			return (this->value.size() > target.value.size());
+		if(a[idxA] < '0'){
+			--a[idxA-1];
+			a[idxA] += 10;
 		}
-
-		for(int i=0; i<this->value.size(); ++i){
-			if(this->value[i] != target.value[i]){
-				return (this->value[i] > target.value[i]);
-			}
-		}
-
-		return true;
 	}
 
-	BigInt& operator++(){
-		this->value[this->value.size() -1] += 1;
-
-		for(int idx = this->value.size()-1; idx >=0; --idx){
-			if(this->value[idx] > '9'){
-				this->value[idx] -= 10;
-				if(idx == 0){
-					this->value = "1" + this->value;
-				}else{
-					this->value[idx-1] += 1;
-				}
-			}
-		}
-
-		return (*this);
-	}
-
-	BigInt& operator-=(const BigInt& target){
-		for(int idxA = this->value.size()-1, idxB = target.value.size()-1; idxB >= 0; --idxA, --idxB){
-			this->value[idxA] = (this->value[idxA] - target.value[idxB]) + '0';
-
-			if(this->value[idxA] < '0'){
-				this->value[idxA] += 10;
-				this->value[idxA -1] -= 1;
-			}
-		}
-
-		for(int idxA = this->value.size()-1; idxA >=0; --idxA){
-			if(this->value[idxA] < '0'){
-				this->value[idxA] += 10;
-				this->value[idxA -1] -= 1;
-			}
-		}
-
-		while(this->value.size() > 1 && this->value[0] == '0'){
-			this->value = this->value.substr(1);
-		}
-
-		return (*this);
-	}
-
-};
+	trim(a);
+}
 
 int main(){
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
 	cout.tie(nullptr);
 
-	BigInt n, m;
-	cin >> n >> m;
+	string a, b;
+	cin >> a >> b;
 
-	BigInt q;
-	q = "0";
+	string quotient = "";
+	string remainder = "";
+	for(int i=0; i<a.length(); ++i){
+		remainder += a[i];
 
-	while(n >= m){
-		n -= m;
-		++q;
+		if(comp(remainder, b)){
+			int cnt = 0;
+			
+			while(comp(remainder, b)){
+				A_subract_B(remainder, b);
+				++cnt;
+			}
+
+			quotient += ('0' + cnt);
+		}else{
+			quotient += '0';
+		}
 	}
 
-	cout << q << '\n' << n;
+	trim(quotient);
+	trim(remainder);
+
+	cout << quotient << '\n' << remainder;
 
 	return 0;
 }
