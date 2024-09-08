b52478b7e465a4f8ae36564d60b8992a8ef3f3e2
dohyeon <businessdohyeon@gmail.com>
2024-09-08 15:44:32 +0900

    #5337

---
+++ #5337.cpp
@@ -0,0 +1,20 @@
+/*
+BackJoon No.
+#5337
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	cout << ".  .   ." << '\n';
+	cout << "|  | _ | _. _ ._ _  _" << '\n';
+	cout << "|/\\|(/.|(_.(_)[ | )(/." << '\n';
+
+	return 0;
+}
