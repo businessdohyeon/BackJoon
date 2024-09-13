0aee945ef95f9c554a0e49c7bfbeae2ca8290cb6
dohyeon <businessdohyeon@gmail.com>
2024-09-13 13:40:20 +0900

    #9653

---
+++ #9653.cpp
@@ -0,0 +1,19 @@
+/*
+BackJoon No.
+#9653
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	cout << "강한친구 대한육군" << '\n';
+	cout << "강한친구 대한육군" << '\n';
+
+	return 0;
+}
