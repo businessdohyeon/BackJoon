85096076ef069678e0d0ad6639a0938bfde2aeae
dohyeon <businessdohyeon@gmail.com>
2024-09-14 22:07:04 +0900

    #15680

---
+++ #15680.cpp
@@ -0,0 +1,25 @@
+/*
+BackJoon No.
+#15680
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int n;
+	cin >> n;
+
+	if(n == 0){
+		cout << "YONSEI";
+	}else{
+		cout << "Leading the Way to the Future";
+	}
+
+	return 0;
+}
