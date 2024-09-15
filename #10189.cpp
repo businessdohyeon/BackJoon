8e664249fc463e4f5a4aa02deaceeef3fefa217b
dohyeon <businessdohyeon@gmail.com>
2024-09-15 22:52:18 +0900

    #10189

---
+++ #10189.cpp
@@ -0,0 +1,21 @@
+/*
+BackJoon No.
+#10189
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	cout << "#  # #### #### #  #" << '\n';
+	cout << "#### #  # #  # # #" << '\n';
+	cout << "#### #  # #  # # #" << '\n';
+	cout << "#  # #### #### #  #" << '\n';
+
+	return 0;
+}
