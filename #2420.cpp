25914eedeffa360d6a7a9f5e8447ee96bfdba326
dohyeon <businessdohyeon@gmail.com>
2024-09-17 23:04:58 +0900

    #2420

---
+++ #2420.cpp
@@ -0,0 +1,21 @@
+/*
+BackJoon No.
+#2420
+*/
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	long long int a, b;
+	cin >> a >> b;
+	cout << abs(a-b);
+
+	return 0;
+}
