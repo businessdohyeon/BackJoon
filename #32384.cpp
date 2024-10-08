f7c2bc3b634bf48c1cca5a840f7c51ee8c8af31f
dohyeon <businessdohyeon@gmail.com>
2024-10-08 23:45:14 +0900

    #32384

---
+++ #32384.cpp
@@ -0,0 +1,23 @@
+/*
+BackJoon No.
+#32384
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int n;
+	cin >> n;
+
+	for(int i=0; i<n; ++i){
+		cout << "LoveisKoreaUniversity" << ' ';
+	}
+
+	return 0;
+}
