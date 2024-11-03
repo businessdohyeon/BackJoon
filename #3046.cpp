481cf5b45e9378f2f4d69b696b3b96e8190d5888
dohyeon <businessdohyeon@gmail.com>
2024-11-03 23:03:06 +0900

    #3046

---
+++ #3046.cpp
@@ -0,0 +1,21 @@
+/*
+BackJoon No.
+#3046
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int r1, s;
+	cin >> r1 >> s;
+
+	cout << 2*s - r1;
+
+	return 0;
+}
