0f47c860be0b4ae7892fb2b7d21346518dbc6ddc
dohyeon <businessdohyeon@gmail.com>
2024-12-13 23:35:41 +0900

    #32651

---
+++ #32651.cpp
@@ -0,0 +1,25 @@
+/*
+BackJoon No.
+#32651
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int n;
+	cin >> n;
+
+	if(n < 100000 && n%2024 == 0){
+		cout << "Yes";
+	}else{
+		cout << "No";
+	}
+
+	return 0;
+}
