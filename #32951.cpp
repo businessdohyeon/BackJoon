b978a35edd0f00cda624b8e1a70d5c4a3f9b3c73
dohyeon <businessdohyeon@gmail.com>
2024-12-23 23:46:16 +0900

    #32951

---
+++ #32951.cpp
@@ -0,0 +1,20 @@
+/*
+BackJoon No.
+#32951
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int n;
+	cin >> n;
+	cout << n - 2024;
+
+	return 0;
+}
