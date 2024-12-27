96e4c3456fb54c88972e9b4c666379ec345f6efc
dohyeon <businessdohyeon@gmail.com>
2024-12-27 23:34:22 +0900

    #15962

---
+++ #15962.cpp
@@ -0,0 +1,18 @@
+/*
+BackJoon No.
+#15962
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	cout << "파이팅!!";
+
+	return 0;
+}
