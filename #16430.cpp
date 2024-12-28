04c2d183fdc3e4b8b28ba9d8f2d46b16d98e090b
dohyeon <businessdohyeon@gmail.com>
2024-12-28 23:25:31 +0900

    #16430

---
+++ #16430.cpp
@@ -0,0 +1,20 @@
+/*
+BackJoon No.
+#16430
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int a, b;
+	cin >> a >> b;
+	cout << b-a << ' ' << b;
+
+	return 0;
+}
